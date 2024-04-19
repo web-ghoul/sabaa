cfa7c5a6ec44d26cc6d33cde00b2c755cea91515
webGhoul <84246173+web-ghoul@users.noreply.github.com>
2024-04-19 13:28:30 +0200

    Pull

--- backend/src/nationality/nationality.service.ts
+++ backend/src/nationality/nationality.service.ts
@@ -1,53 +1,68 @@
 import { HttpException, HttpStatus, Injectable } from '@nestjs/common';
-import { CreateNationalityDto } from './dto/create-nationality.dto';
-import { UpdateNationalityDto } from './dto/update-nationality.dto';
 import { InjectModel } from '@nestjs/mongoose';
 import { Model } from 'mongoose';
 import { Nationality } from 'schemas/nationality.schema';
+import { CreateNationalityDto } from './dto/create-nationality.dto';
+import { UpdateNationalityDto } from './dto/update-nationality.dto';
 
 @Injectable()
 export class NationalityService {
-  constructor(@InjectModel(Nationality.name) private nationalityModel: Model<Nationality>) {}
+  constructor(
+    @InjectModel(Nationality.name) private nationalityModel: Model<Nationality>,
+  ) {}
 
-  async create(createNationalityDto: CreateNationalityDto): Promise<Nationality> {
-    try{
+  async create(
+    createNationalityDto: CreateNationalityDto,
+  ): Promise<Nationality> {
+    try {
       const nationality = new this.nationalityModel(createNationalityDto);
       return await nationality.save();
-    }catch(err)
-    {
-      throw new HttpException("Error while creating nationality" , HttpStatus.FORBIDDEN);
+    } catch (err) {
+      console.log(err);
+
+      throw new HttpException(
+        'Error while creating nationality',
+        HttpStatus.FORBIDDEN,
+      );
     }
   }
 
-  findAll(limit: number, page: number, search:string,fields:string[],sortType:string, deleted:boolean = false): Promise<Nationality[]> {  
+  findAll(
+    limit: number,
+    page: number,
+    search: string,
+    fields: string[],
+    sortType: string,
+    deleted: boolean = false,
+  ): Promise<Nationality[]> {
     const projection: any = {};
     if (fields && fields.length > 0) {
-        fields.forEach(field => {
-            projection[field] = 1; // Include the field
-        });
+      fields.forEach((field) => {
+        projection[field] = 1; // Include the field
+      });
     }
 
-    const sort:any = {}
-    if(sortType == "nationality_asc")
-    {
-      sort["nationality"] = 1; 
-    }else if(sortType == "code_asc")
-    {
-      sort["id"] = 1; 
-    }else
-    {
-      sort["createdAt"] = -1; 
+    const sort: any = {};
+    if (sortType == 'nationality_asc') {
+      sort['nationality'] = 1;
+    } else if (sortType == 'code_asc') {
+      sort['id'] = 1;
+    } else {
+      sort['createdAt'] = -1;
     }
-   
-    
-    return this.nationalityModel.find({
-  $or: [
-    { nationality: { $regex: new RegExp(search, "i") } },
-    { id: { $regex: new RegExp(search, "i") } }
-  ],
-  deleted : deleted
-}).select(projection).limit(limit).skip(page * limit).sort(sort);
 
+    return this.nationalityModel
+      .find({
+        $or: [
+          { nationality: { $regex: new RegExp(search, 'i') } },
+          { id: { $regex: new RegExp(search, 'i') } },
+        ],
+        deleted: deleted,
+      })
+      .select(projection)
+      .limit(limit)
+      .skip(page * limit)
+      .sort(sort);
   }
 
   findOne(id: string) {
@@ -55,42 +70,50 @@ export class NationalityService {
   }
 
   async update(id: string, updateNationalityDto: UpdateNationalityDto) {
-    try{
-      return await this.nationalityModel.findByIdAndUpdate(id, updateNationalityDto);
-    }catch(err)
-    {
-      throw new HttpException("Error while updating nationality" , HttpStatus.FORBIDDEN);
+    try {
+      return await this.nationalityModel.findByIdAndUpdate(
+        id,
+        updateNationalityDto,
+      );
+    } catch (err) {
+      throw new HttpException(
+        'Error while updating nationality',
+        HttpStatus.FORBIDDEN,
+      );
     }
   }
 
   remove(id: string) {
-    try{
-      return this.nationalityModel.updateOne({ _id: id },{deleted : true});
-
-    }catch(err)
-    {
-      throw new HttpException("Error while deleting nationality" , HttpStatus.FORBIDDEN);
+    try {
+      return this.nationalityModel.updateOne({ _id: id }, { deleted: true });
+    } catch (err) {
+      throw new HttpException(
+        'Error while deleting nationality',
+        HttpStatus.FORBIDDEN,
+      );
     }
   }
 
   importNationalities(createNationalityDto: CreateNationalityDto[]) {
-    try{
-      
+    try {
       return this.nationalityModel.insertMany(createNationalityDto);
-    }catch(err)
-    {
-      throw new HttpException({
-        status: HttpStatus.FORBIDDEN,
-        error: err,
-      }, HttpStatus.FORBIDDEN, {
-        cause: err
-      });
+    } catch (err) {
+      throw new HttpException(
+        {
+          status: HttpStatus.FORBIDDEN,
+          error: err,
+        },
+        HttpStatus.FORBIDDEN,
+        {
+          cause: err,
+        },
+      );
     }
-    
   }
   async getCounters() {
     const count = await this.nationalityModel.estimatedDocumentCount();
     return {
-      count}
+      count,
+    };
   }
 }
